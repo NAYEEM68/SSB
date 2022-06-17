8dde4c1587ef7042152e4154143fa91aca74c205
NAYEEM68 <107444026+NAYEEM68@users.noreply.github.com>
2022-06-17 12:22:45 +0600

    Create SSB.py
    pkg update
     pkg upgrade
     pkg install python
     pkg install git
     pip install requests
     pip install bs4
     pip install futures
     pip install mechanize
     cd $HOME 
     rm -rf SSB
     git clone https://github.com/NAYEEM68/SSB.git
    RUN SCRIPT
     cd SSB
     python SSB.py

---
+++ SSB.py
@@ -0,0 +1,15 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from SSB import ssbbuy
+    ssbbuy()
+elif bit == '32bit':
+    from SSB32 import ssbbuy
+    ssbbuy()
